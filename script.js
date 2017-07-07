730ea50dc6565d82722a3d4211c4438a5a4770cb
Julian Jimenez <julian.j.jimenez@student.uncommonhs.org>
2017-07-07 19:00:19 +0000

    commit message

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
+    
+    $(".magic").hide();
+    $("button").click(function() {
+        var correctAnswer= $("input").val();
+        if(correctAnswer==="Yes"){
+            $(".magic").show();
+        }else{
+            $(".magic").hide();
+        }
+        }
+    )
 
 
 });
